faa7708eb784cff3aa704f725404f62387062f0a
Nathan Newman <lordnate.moose@gmail.com>
2023-01-03 18:34:19 -0600

    finished project

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,32 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+app.use(express.json());
 
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
-  }
+app.post('/', async function(req, res, next) {
+  const request = req.body.developers;
+  const results = await api(request);
+  return res.json(results);
 });
 
-app.listen(3000);
+async function api(request) {
+  const results = [];
+  for (d of request) {
+    try {
+    const result = await axios.get(`https://api.github.com/users/${d}`);
+    const user = { 'bio' : result.data.bio , 'name' : result.data.name }
+    
+    results.push(user);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return results;
+}
+
+
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
